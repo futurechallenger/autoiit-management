488d95db3ebe6a716092e047bf6c9f9853e2fac7
Uncle Charlie <uncle_charlie@maddog.com>
2024-12-25 17:30:27 +0800

    add navbar

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Autoiit",
+  description: "Autoiit management system",
+  navItems: [
+    { label: "Home", href: "/" },
+    { label: "Users", href: "/users" },
+  ],
+};
