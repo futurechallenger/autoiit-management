9e7500bfba43fa264ac2e978be2ecc10356857a9
Uncle Charlie <uncle_charlie@maddog.com>
2024-12-28 21:55:03 +0800

    debug fullstack nextjs project

--- services/userService.ts
+++ services/userService.ts
@@ -1,10 +1,7 @@
-"use server";
-
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "@/lib/prisma";
 
 async function getUsers() {
+  console.log('getUsers');
   try {
     const users = await prisma.userInfo.findMany();
     return users;
